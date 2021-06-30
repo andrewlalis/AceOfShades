3b9519cbcab3fe038da633a4404fbf8f8380c914
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-06-30 19:28:01 +0200

    Fixed POM files and updated package.bat.

--- client/package.bat
+++ client/package.bat
@@ -3,7 +3,7 @@ jpackage^
  --type exe^
  --name "Ace-of-Shades"^
  --description "Top-down 2D shooter game inspired by Ace of Spades."^
- --module-path "aos-client-4.0.jar;../../core/target/aos-core-4.0.jar"^
+ --module-path "aos-client-5.0.jar;../../core/target/aos-core-5.0.jar"^
  --module aos_client/nl.andrewlalis.aos_client.launcher.Launcher^
  --win-shortcut^
  --win-dir-chooser
